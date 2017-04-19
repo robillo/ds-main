bd88bbb9a5b0df7d63b3120144328b8d27ea4b67
Rishabh Shukla <rishabhshukla25@hotmail.com>
2017-04-19 20:54:19 +0530

    register screen layout

--- app/src/main/java/com/example/sasuke/dailysuvichar/fragment/LoginFragment.java
+++ app/src/main/java/com/example/sasuke/dailysuvichar/fragment/LoginFragment.java
@@ -1,11 +1,13 @@
 package com.example.sasuke.dailysuvichar.fragment;
 
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -20,7 +22,6 @@ import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.login.LoginResult;
-import com.facebook.login.widget.DeviceLoginButton;
 import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -35,13 +36,15 @@ import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.mobsandgeeks.saripaar.Validator;
 import com.mobsandgeeks.saripaar.annotation.Email;
 import com.mobsandgeeks.saripaar.annotation.NotEmpty;
 
 import butterknife.BindView;
-import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 /**
@@ -68,6 +71,8 @@ public class LoginFragment extends BaseFragment implements GoogleApiClient.OnCon
     private static final int RC_GSIGN_IN = 9001;
     private static final String TAG = "STATUS";
     private GoogleApiClient mGoogleApiClient;
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference;
     private FirebaseAuth mFirebaseAuth;
     private CallbackManager callbackManager;
 
@@ -84,7 +89,7 @@ public class LoginFragment extends BaseFragment implements GoogleApiClient.OnCon
         mFirebaseAuth = FirebaseAuth.getInstance();
 
         callbackManager = CallbackManager.Factory.create();
-
+        databaseReference = FirebaseDatabase.getInstance().getReference();
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail().build();
@@ -121,7 +126,77 @@ public class LoginFragment extends BaseFragment implements GoogleApiClient.OnCon
 
     @OnClick(R.id.btn_login)
     public void login() {
+//        startActivity(HomeActivity.newIntent(getContext()));
+        FirebaseSignIn();
+    }
+    private void FirebaseSignIn() {
+        if (!validateForm()) {
+            return;
+        }
+
+        //showProgressDialog();
+        String email = mEtEmail.getText().toString();
+        String password = mEtPassword.getText().toString();
+
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener((Activity) getContext(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signIn:onComplete:" + task.isSuccessful());
+//                         hideProgressDialog();
+
+                        if (task.isSuccessful()) {
+                            onAuthSuccess(task.getResult().getUser());
+                        } else {
+                            Toast.makeText(getContext(), "Sign In Failed",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+    }
+
+    private void onAuthSuccess(FirebaseUser user) {
+        String username = usernameFromEmail(user.getEmail());
+
+        // Write new user
+        writeNewUser(user.getUid(), username, user.getEmail());
+
         startActivity(HomeActivity.newIntent(getContext()));
+
+    }
+
+    private String usernameFromEmail(String email) {
+        if (email.contains("@")) {
+            return email.split("@")[0];
+        } else {
+            return email;
+        }
+    }
+
+    private void writeNewUser(String userId, String name, String email) {
+//        User user = new User(email, name);
+//
+//        databaseReference.child("users").child(userId).setValue(user);
+    }
+
+    private boolean validateForm() {
+        boolean result = true;
+        if (TextUtils.isEmpty(mEtEmail.getText().toString())) {
+            mEtEmail.setError("Required");
+            result = false;
+        } else {
+            mEtEmail.setError(null);
+        }
+
+        if (TextUtils.isEmpty(mEtPassword.getText().toString())) {
+            mEtPassword.setError("Required");
+            result = false;
+        } else {
+            mEtPassword.setError(null);
+        }
+
+        return result;
     }
 
     @OnClick(R.id.google_sign_in)
---
+++ app/src/main/java/com/example/sasuke/dailysuvichar/fragment/RegisterFragment.java
@@ -0,0 +1,12 @@
+package com.example.sasuke.dailysuvichar.fragment;
+
+/**
+ * Created by rishabhshukla on 19/04/17.
+ */
+
+public class RegisterFragment extends BaseFragment {
+    @Override
+    protected int getLayoutResId() {
+        return 0;
+    }
+}
