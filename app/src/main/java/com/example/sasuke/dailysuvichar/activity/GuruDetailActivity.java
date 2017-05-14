4c638f46227c0a3105ab71db87c2872db6ef0d16
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-05-14 23:13:48 +0530

    dp and cover.

--- app/src/main/java/com/example/sasuke/dailysuvichar/activity/GuruDetailActivity.java
+++ app/src/main/java/com/example/sasuke/dailysuvichar/activity/GuruDetailActivity.java
@@ -1,16 +1,45 @@
 package com.example.sasuke.dailysuvichar.activity;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.sasuke.dailysuvichar.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
+import jp.wasabeef.glide.transformations.gpu.BrightnessFilterTransformation;
+import jp.wasabeef.glide.transformations.gpu.VignetteFilterTransformation;
 
 public class GuruDetailActivity extends BaseActivity{
 
+    private static final int RESULT_LOAD_IMAGE = 8008, RESULT_LOAD_COVER = 8009;
     private Context context;
+
+    @BindView(R.id.recyclerview)
+    RecyclerView recyclerView;
+    @BindView(R.id.name)
+    TextView name;
+    @BindView(R.id.dp)
+    ImageView dp;
+    @BindView(R.id.cover)
+    ImageView cover;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -19,5 +48,81 @@ public class GuruDetailActivity extends BaseActivity{
         ButterKnife.bind(this);
 
         context = getApplicationContext();
+
+        if(ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 5);
+        }
+
+        cover.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, RESULT_LOAD_COVER);
+            }
+        });
+
+        dp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.e("Insert Requested", "YES!.");
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            Log.e("REQUEST BEFORE", picturePath);
+//            Glide.with(this)
+//                    .load(picturePath)
+//                    .crossFade()
+//                    .centerCrop()
+//                    .into(userProfilePic);
+
+            Bitmap bmImg = BitmapFactory.decodeFile(picturePath);
+            dp.setImageBitmap(bmImg);
+
+            Log.e("REQUEST AFTER", picturePath);
+        }
+        else if(requestCode == RESULT_LOAD_COVER && resultCode == RESULT_OK && null != data){
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+//            Glide.with(this)
+//                    .load(picturePath)
+//                    .crossFade()
+//                    .centerCrop()
+//                    .into(userCoverPic);
+
+            Bitmap bmImg = BitmapFactory.decodeFile(picturePath);
+            cover.setImageBitmap(bmImg);
+        }
+
     }
 }
