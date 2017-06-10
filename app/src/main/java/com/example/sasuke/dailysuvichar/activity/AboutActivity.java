f8afbf2ff053c738da7dba788ec56b7759df43b2
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-06-10 08:40:00 +0530

    erroneos back press toolbar scroll fixed

--- app/src/main/java/com/example/sasuke/dailysuvichar/activity/AboutActivity.java
+++ app/src/main/java/com/example/sasuke/dailysuvichar/activity/AboutActivity.java
@@ -12,4 +12,9 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
     }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
 }
